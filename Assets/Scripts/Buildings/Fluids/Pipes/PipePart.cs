a4997128f307b6349ab924fd8c7fc15e972b1cc6
Richard Kopp <risa.kopp@gmail.com>
2024-09-04 20:10:17 +0200

    Cleanup

--- Assets/Scripts/Buildings/Fluids/Pipes/PipePart.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
-
-public class PipePart : ClickableObject
-{
-    public Pipe connectedPipe; // not parent but the next pipe
-}
