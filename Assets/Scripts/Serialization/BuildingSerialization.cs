78776d667cf0de6f12159f745eb3a99ad557bd92
IsaKobe <risa.kopp@gmail.com>
2025-06-16 12:57:56 +0200

    Stats working

--- Assets/Scripts/Serialization/BuildingSerialization.cs
+++ Assets/Scripts/Serialization/BuildingSerialization.cs
@@ -1,14 +1,9 @@
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Net.Http.Json;
 using System.Text;
 using UnityEditor;
-using UnityEditor.PackageManager;
 using UnityEngine;
 public class BuildingSerialization
 {
